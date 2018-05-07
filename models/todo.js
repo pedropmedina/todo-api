92c0ca78708ae5ea2c666f8c1fb1d086e33f5f54
pedropmedina <pedrodelilia@gmail.com>
2018-05-07 19:23:41 -0400

    Added Todo model

---
+++ models/todo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const Joi = require('joi');
+
+const todoSchema = new Schema({
+	text: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 255,
+		trim: true,
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+const validateTodo = todo => {
+	const schema = {
+		text: Joi.string()
+			.min(5)
+			.max(255)
+			.required(),
+		completed: Joi.boolean(),
+	};
+};
+
+module.exports = { Todo, validateTodo };
