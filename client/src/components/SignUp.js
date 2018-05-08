f993d75d4e138bad1112a9f9657b1ac442b66501
pedropmedina <pedrodelilia@gmail.com>
2018-05-08 18:35:27 -0400

    Styled SignUp component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,7 +1,105 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const SignUp = () => {
-	return <div>hello there from SignUp component</div>;
-};
+const Form = styled.form`
+	width: 50rem;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	padding: 4rem 6rem;
+	background-color: #dedede;
+
+	> * {
+		width: 100%;
+	}
+
+	> input {
+		height: 5rem;
+		text-indent: 1rem;
+		font-size: 1.6rem;
+		border: none;
+		border-bottom: 0.2rem solid #fefefe;
+		background-color: inherit;
+		color: #fefefe;
+		outline: none;
+		position: relative;
+
+		&::placeholder {
+			color: #fefefe;
+		}
+
+		&:focus {
+			background-color: #cecece;
+		}
+
+		&:focus + div::after {
+			transform: scaleX(1);
+			background-color: #318c8c;
+		}
+	}
+
+	> div {
+		width: 100%;
+		background-color: inherit;
+		height: 0.5rem;
+		display: block;
+		margin-bottom: 2rem;
+		position: relative;
+
+		&::after {
+			content: '';
+			position: absolute;
+			width: 100%;
+			height: 0.3rem;
+			margin-top: -0.2rem;
+			background-color: transparent;
+			transform: scaleX(0);
+			transform-origin: center;
+			transition: all 0.3s;
+		}
+	}
+
+	> button {
+		padding: 1.5rem;
+		border: none;
+		background-color: #aeaeae;
+		color: #fefefe;
+		font-size: 1.6rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1rem;
+		margin-top: 1rem;
+	}
+`;
+
+class SignUp extends React.Component {
+	state = {
+		fields: {
+			name: '',
+			email: '',
+			password: '',
+		},
+		errFields: {},
+	};
+
+	onSubmitForm = () => {
+		// code here
+	};
+
+	render() {
+		const { name, email, password } = this.state.fields;
+		return (
+			<Form action="#" onSubmit={this.onSubmitForm}>
+				<input type="text" placeholder="name" value={name} />
+				<div />
+				<input type="text" placeholder="email" value={email} />
+				<div />
+				<input type="text" placeholder="password" value={password} />
+				<div />
+				<button>Sign up</button>
+			</Form>
+		);
+	}
+}
 
 export default SignUp;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routes/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+	html {
+		font-size: 62.5%;
+	}
+
+	body {
+		font-family: 'Helvetica';
+		box-sizing: border-box;
+		line-height: 1.3;
+	}
+
+	*,
+	*::before,
+	*::after {
+		margin: 0;
+		padding: 0;
+		box-sizing: inherit;
+	}
+`;
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 registerServiceWorker();
